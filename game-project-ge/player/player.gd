ad939786b90aed1c4bf8c68c7d7533fb17e00106
SomeDude <thanaphumi.t@kkumail.com>
2026-09-07 16:15:23 +0700

    add model wrench, fuse, box and prepare scene

--- game-project-ge/player/player.gd
+++ game-project-ge/player/player.gd
@@ -23,8 +23,8 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#	velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
